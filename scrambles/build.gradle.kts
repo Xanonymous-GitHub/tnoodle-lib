3841337770cef19d754294df4e9930ee480caa80
Xanonymous <trusaidlin@gmail.com>
2026-01-23 13:28:25 +0800

    feat: 🎸 enable kotlin support

--- scrambles/build.gradle.kts
+++ scrambles/build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     `maven-publish`
     signing
     alias(libs.plugins.shadow)
+    kotlin("jvm")
 }
 
 attachRemoteRepositories()
@@ -35,6 +36,7 @@ dependencies {
     testImplementation(libs.junit.jupiter.engine)
 
     testRuntimeOnly(libs.junit.platform.launcher)
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 configureJUnit5()
@@ -49,4 +51,10 @@ tasks.shadowJar {
     // https://github.com/GradleUp/shadow/issues/111
     isPreserveFileTimestamps = false
     isReproducibleFileOrder = true
+}
+repositories {
+    mavenCentral()
+}
+kotlin {
+    jvmToolchain(25)
 }
