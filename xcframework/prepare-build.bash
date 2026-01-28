aab2f4919ff7df398ea4ac041a73b40718126b58
Xanonymous <trusaidlin@gmail.com>
2026-01-28 22:31:27 +0800

    fix: 🐛 should create dir

--- xcframework/prepare-build.bash
+++ xcframework/prepare-build.bash
@@ -9,7 +9,8 @@ if [[ -n "${JAVA_HOME:-}" ]]; then
   echo "JAVA_HOME=$JAVA_HOME"
 fi
 
-./gradlew -q showClassPath > build/apple-classpath.txt
+mkdir -p build
+./gradlew -q showClassPath >| build/apple-classpath.txt
 
 # For iOS/macOS native build we intentionally do NOT translate third-party libs
 # If third-party libs stays on the classpath, it will drag in third-party libs-only sources and break translation.
